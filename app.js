77072882f6d5f905050048ddb7750ac5b2e1cdf3
kimseasok <basicblogtalk@gmail.com>
2018-12-29 00:05:52 -0500

    Clean up the codes

--- app.js
+++ app.js
@@ -16,44 +16,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!...');
 });
 
-var bookRouter = express.Router();
+bookRouter = require('./routes/bookRoutes')(Book);
 
-bookRouter.route('/books')
-    .post((req, res) => {
-        var book = new Book(req.body);
 
-        book.save();
-
-        res.status(201).send(book);
-    })
-    .get((req, res) => {
-        var query = {};
-
-        if (req.query.genre) {
-            query.genre = req.query.genre;
-        }
-
-        Book.find(query, (err, books) => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.json(books);
-            }
-        });
-    });
-
-bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-        Book.findById(req.params.bookId, (err, book) => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.json(book);
-            }
-        });
-    });
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
+app.use('/api/authors', bookRouter);
 
 app.listen(port, () => {
     console.log(`Gulp is running on PORT: ${port}`);
---
+++ routes/bookRoutes.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+var route = (Book) => {
+  var bookRouter = express.Router();
+
+  bookRouter.route('/')
+    .post((req, res) => {
+      var book = new Book(req.body);
+
+      book.save();
+
+      res.status(201).send(book);
+    })
+    .get((req, res) => {
+      var query = {};
+
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      }
+
+      Book.find(query, (err, books) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(books);
+        }
+      });
+    });
+
+  bookRouter.route('/:bookId')
+    .get((req, res) => {
+      Book.findById(req.params.bookId, (err, book) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(book);
+        }
+      });
+    });
+
+  return bookRouter;
+};
+
+module.exports = route;
