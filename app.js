c08e0fef3aef9582a5b2452608903140ec55e942
kimseasok <basicblogtalk@gmail.com>
2018-12-28 22:48:31 -0500

    Add mongo Model and filtering with Query string

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose');
 
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 var app = express();
-
 var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to node API');
-});
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get((req, res) => {
+        var query = {};
+
+        if (req.query.genre) {
+            query.genre = req.query.genre;
+        }
+
+        Book.find(query, (err, books) => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(books);
+            }
+        });
+    })
+    .post()
+
+app.use('/api', bookRouter);
 
 app.listen(port, () => {
     console.log(`Gulp is running on PORT: ${port}`);
