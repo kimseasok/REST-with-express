6e65bcf547c22d0c3db68a9f65ee91af079ea658
kimseasok <basicblogtalk@gmail.com>
2018-12-29 23:00:28 -0500

    Fix middleware and add patch

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -33,7 +33,6 @@ var route = (Book) => {
         res.status(500).send(err);
       } else if (book) {
         req.book = book;
-        res.json(book);
         next();
       } else {
         res.status(404).send('Not found');
@@ -45,20 +44,32 @@ var route = (Book) => {
       res.json(req.book);
     })
     .put((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
+      req.book.title = req.body.title;
+      req.book.genre = req.body.genre;
+      req.book.author = req.body.author;
+      req.book.read = req.body.read;
+      req.book.save((err) => {
         if (err) {
-          res.status(500).send(err);
+          res.status(500).send(err)
         } else {
-          req.book.title = req.body.title;
-          req.book.genre = req.body.genre;
-          req.book.author = req.body.author;
-          req.book.read = req.body.read;
-          req.book.save();
-          res.json(book);
+          res.json(req.book);
         }
       });
     })
-    .patch();
+    .patch((req, res) => {
+      if (req.body._id)
+        delete req.body._id;
+      for (var p in req.body) {
+        req.book[p] = req.body[p];
+      }
+      req.book.save((err) => {
+        if (err) {
+          res.status(500).send(err)
+        } else {
+          res.json(req.book);
+        }
+      });
+    });
 
   return bookRouter;
 };
