d1419c7899eefa60c4aba7a4087e849847b86c46
kimseasok <basicblogtalk@gmail.com>
2018-12-29 22:21:06 -0500

    Add minddleware to bookRoutes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -27,30 +27,38 @@ var route = (Book) => {
       });
     });
 
+  bookRouter.use('/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        res.status(500).send(err);
+      } else if (book) {
+        req.book = book;
+        res.json(book);
+        next();
+      } else {
+        res.status(404).send('Not found');
+      }
+    });
+  });
   bookRouter.route('/:bookId')
     .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          res.json(book);
-        }
-      });
+      res.json(req.book);
     })
     .put((req, res) => {
       Book.findById(req.params.bookId, (err, book) => {
         if (err) {
           res.status(500).send(err);
         } else {
-          book.title = req.body.title;
-          book.genre = req.body.genre;
-          book.author = req.body.author;
-          book.read = req.body.read;
-          book.save();
+          req.book.title = req.body.title;
+          req.book.genre = req.body.genre;
+          req.book.author = req.body.author;
+          req.book.read = req.body.read;
+          req.book.save();
           res.json(book);
         }
       });
-    });
+    })
+    .patch();
 
   return bookRouter;
 };
